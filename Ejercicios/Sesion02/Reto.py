326a6f296ed6dd5eede4e308fb082cb4bae2c723
Javier Calvo <javiercalvoporro@gmail.com>
2024-03-07 19:48:26 +0100

    Reto hecho y Orientacion a objetos

---
+++ Ejercicios/Sesion02/Reto.py
@@ -0,0 +1,43 @@
+#Ejercicio de Reto
+
+fichero = r"Ejercicios\Sesion02\reto.txt"
+mode = 'r'
+
+try:
+
+    f = open(fichero, mode)
+    fichero = f.readlines()
+
+    def reto(fichero,as1,as2):
+        cent = 0
+        as1List = []
+        asign = []
+        for linea in fichero:
+            #Eliminamos el \n de cada elemento
+            l = linea.strip().split(' ')
+            if l[0] not in asign:
+                asign.append(l[0])
+            #Comprobamos si esa asignatura
+            if l[0] == as1:
+                as1List.append(l[1])   
+        for linea in fichero:
+            l = linea.strip().split(' ')
+            if l[0] == as2:
+                if l[1] in as1List:
+                    cent += 1
+        return cent,asign
+
+    #Eleccion de 2 Asignaturas
+    print("Elige dos asignaturas")
+    a1 = input("Primera: ")
+    a2 = input("SEgunda: ")
+
+    #Comprobacion de las asignaturas
+    l = reto(fichero, a1.upper(), a2.upper())
+    if a1.upper() in l[1] and a2.upper() in l[1]:
+        print("Total alumnos:", l[0])
+    else:
+        print("Las asignaturas no son correctas")
+
+except FileNotFoundError:
+    print("Fichero no encontrado")
--- Teoria/Sesion02/OrientacionObjetosI.py
+++ Teoria/Sesion02/OrientacionObjetosI.py
@@ -18,3 +18,56 @@ coche.cv = 150
 print(coche.cv)
 
 #Esta no es la mejor manera de crear una clase en Python. Debe ser atribuyendo el self (__init__)
+
+#Ahora creamos una que es la que se suele usar y es la recomendable.
+class Persona():
+    #Clase Persona que contiene parametros como altura,edad y profesion
+    #Contiene funciones para cambiar estos parametros y para imprimirlo por pantalla
+    def __init__(self):
+        self.__altura = 0
+        self.__edad = 0
+        self.__profesion = ""
+        return None
+    
+    def setAltura(self,alt):
+        if alt<0 or alt>260:
+            print("Altura Invalida (0 -260 cm)")
+        else:
+            self.__altura = alt 
+            return None 
+
+    def setEdad(self,edad):
+        if edad<0 or edad>130:
+            print("Rango de edad invalido (0 - 130)")
+        else:
+            self.__edad = edad
+            return None
+    
+    def setProfesion(self,profesion):
+        self.__profesion = profesion
+        return None
+    
+    def getAtributes(self):
+        return self.__altura, self.__profesion, self.__edad
+
+#Hemos generado una clase mas completa donde podemos modificarle las cosas.
+#La clase por tiene valores por defecto, asi a la hora de crear un objeto debemos de modificarle
+    #los atributos que tiene
+        
+#Creacion de un objeto
+Javier = Persona()
+
+#Uso de una funcion dentro de el
+print(Javier.getAtributes())
+#Como vemos lo devuelve en una tupla, por lo que podemos devolverlo mas formal con un bucle..
+
+#Asignacion de atributos
+#Atributo valido
+edad = input("Dime la edad de Javier: ")
+Javier.setEdad(int(edad))
+
+#Atributo invalido
+edad = input("Dime la edad de Javier: ")
+Javier.setEdad(int(edad))
+
+
